a313e46af28019238004d01df61b99c82e734819
stefangohlke <gohlke.stefan@yahoo.de>
2023-02-08 17:17:35 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export {
    GamePlayer,
    PlayerAgainstAIGame,
    randomCounterAttackFunction,
-} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.5.1/src/index.ts';
-export { serve } from 'https://deno.land/std@0.170.0/http/server.ts';
+} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.5.2/src/index.ts';
+export { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
