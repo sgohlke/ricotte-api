60a3760a138a3ba667bc50b4ea635460db9fce91
sgohlke <gohlke.stefan@yahoo.de>
2023-07-08 15:18:04 +0200

    Update rpg-ts

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export {
    GamePlayer,
    PlayerAgainstAIGame,
    randomCounterAttackFunction,
-} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.5.3/src/index.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.6.0/src/index.ts'
 export { extractAccessTokenFromAuthHeader } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/request/extractAccessTokenFromAuthHeader.ts'
 export {
    logAndReturnErrorResponse,
