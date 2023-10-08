e9523d70b608d009d44135cbba66fafa40a04c59
sgohlke <gohlke.stefan@yahoo.de>
2023-10-08 15:53:32 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -4,10 +4,10 @@ export {
    PlayerAgainstAIGame,
    randomCounterAttackFunction,
 } from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.8.1/src/index.ts'
-export { extractAccessTokenFromAuthHeader } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/request/extractAccessTokenFromAuthHeader.ts'
+export { extractAccessTokenFromAuthHeader } from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/request/extractAccessTokenFromAuthHeader.ts'
 export {
    logAndReturnErrorResponse,
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/response/ResponseHeaders.ts'
-export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/server/serverFunctions.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/response/ResponseHeaders.ts'
+export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/server/serverFunctions.ts'
