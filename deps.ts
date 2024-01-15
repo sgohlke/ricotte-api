9d65578763fa7b8c08f43a887e1334a347130deb
sgohlke <gohlke.stefan@yahoo.de>
2024-01-15 21:38:26 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -3,13 +3,13 @@ export {
    InMemoryPlayerDataStore,
    PlayerAgainstAIGame,
    randomCounterAttackFunction,
-} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.10.0/src/index.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.11.0/src/index.ts'
 export {
    KVPlayerDataStore,
 } from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-kv/v0.1.0/src/index.ts'
-export { extractAccessTokenFromAuthHeader } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.0.0/request/extractAccessTokenFromAuthHeader.ts'
+export { extractAccessTokenFromAuthHeader } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/request/extractAccessTokenFromAuthHeader.ts'
 export {
    logAndReturnErrorResponse,
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.0.0/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.0.0/response/ResponseHeaders.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/response/ResponseHeaders.ts'
