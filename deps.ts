d496499f5649fd8bec03aaffaff1a0062c5a57ab
sgohlke <gohlke.stefan@yahoo.de>
2024-05-06 23:47:22 +0200

    Update deps.

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export {
    PlayerAgainstAIGame,
    randomCounterAttackFunction,
    SPDTurnBar
-} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.12.0/src/index.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/rpg-ts/v0.12.1/src/index.ts'
 export {
    KVPlayerDataStore,
 } from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-kv/v0.1.0/src/index.ts'
@@ -14,4 +14,4 @@ export {
    JSON_CONTENT_TYPE_HEADER,
    logAndReturnErrorResponse,
    returnDataResponse
-} from 'jsr:@sgohlke/deno-web@^8.2.3'
+} from 'jsr:@sgohlke/deno-web'
